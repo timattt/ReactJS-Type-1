2956b51a745adefa5c57cb0abb5b8d0aa9a81f4f
timattt <timattt@gmail.com>
2023-12-11 11:36:45 +0300

    simple tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import AppWrapper from "../App";
 import '@testing-library/jest-dom'
 
@@ -6,4 +6,17 @@ test('home', () => {
   render(<AppWrapper />);
   const linkElement = screen.getAllByText(/Home/);
   expect(linkElement[0]).toBeInTheDocument()
+});
+
+test('login', () => {
+    render(<AppWrapper />);
+    fireEvent(
+        screen.getByText(/Login/),
+        new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+        }),
+    )
+    const linkElement = screen.getAllByText(/Name/);
+    expect(linkElement[0]).toBeInTheDocument()
 });
