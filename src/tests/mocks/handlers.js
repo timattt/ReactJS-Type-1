770feced9e5aa19974e189fae88e51abb79eefd6
timattt <timattt@gmail.com>
2023-12-11 11:25:40 +0300

    cleanup

--- src/tests/mocks/handlers.js
+++
@@ -1,8 +0,0 @@
-import { http, HttpResponse  } from 'msw'
-
-export const handlers = [
-    http.get('/api/data', () => {
-        // Simulate a successful response with mock data
-        return HttpResponse.json({token: "ggg"})
-    }),
-];
